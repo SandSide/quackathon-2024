6f719482a930d2c12b23e327de2b1046fd6111a2
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2024-03-09 14:40:39 +0000

    Create TurnsSystem.js

---
+++ TurnsSystem.js
@@ -0,0 +1,26 @@
+var yourTurn = true;
+var gameOver = false;
+
+while(!gameOver)
+{
+    if(yourTurn)
+    {
+        //Player Actions will go here
+
+        
+
+
+
+        yourTurn = !yourTurn;
+    }
+    else
+    {
+        //Enemy actions will go here
+
+
+
+
+
+        yourTurn = !yourTurn;
+    }
+}

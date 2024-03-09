ceda7f551d12bb349fe5e5fa33e7d76a7ff3bc76
Stoib <iainbatman@gmail.com>
2024-03-09 20:55:02 +0000

    Created a menu which will start the game

--- game.js
+++ game.js
@@ -11,7 +11,36 @@ window.onload = async () => {
     d3.select(currNode)
     .style('fill', 'red')
     .attr('r', 10)
-    .attr('z', -10); 
+    .attr('z', -10)
+    .attr('i', 0); 
+    calculateMoves(currNode);
+}
+
+function drawLine(a,b){
+
+    d3.selectAll(".line")
+        .data(data)
+        .enter()
+        .append("line")
+        .attr("x1", () => projection([a.long, a.lat])[0])
+        .attr("y1", () => projection([a.long, a.lat])[1])
+        .attr("x2", () => projection([b.long, b.lat])[0])
+        .attr("y2", () => projection([b.long, b.lat])[1])
+        .style('stroke', 'blue')
+        .style('stroke-width', .5);  
+}
+
+function infect(currNode){
+    if(currNode.i == true){console.log("we in")}
+}
+
+// Determine possible movements
+
+// Choose/ Move to position
+
+// Determine action
+
+function calculateMoves(currNode){
 
     var maxDist = 10;
 
@@ -31,32 +60,10 @@ window.onload = async () => {
 
                 d3.select(nodes[i])
                 .style('fill', 'yellow'); 
-
+                
                 //drawLine(currData)
             }
 
         }   
     }
 }
-
-function drawLine(a,b){
-
-    d3.selectAll(".line")
-        .data(data)
-        .enter()
-        .append("line")
-        .attr("x1", () => projection([a.long, a.lat])[0])
-        .attr("y1", () => projection([a.long, a.lat])[1])
-        .attr("x2", () => projection([b.long, b.lat])[0])
-        .attr("y2", () => projection([b.long, b.lat])[1])
-        .style('stroke', 'blue')
-        .style('stroke-width', .5);  
-}
-
-
-
-// Determine possible movements
-
-// Choose/ Move to position
-
-// Determine action
